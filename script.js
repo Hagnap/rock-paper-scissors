b3129ca37044f81396b2fac1bfd1fa5685417194
jhagg26 <jhagg26@gmail.com>
2023-04-15 06:28:49 -0400

    Add getPlayersChoice function

--- script.js
+++ script.js
@@ -17,10 +17,38 @@ function getComputerChoice() {
     }
 }
 
+function getPlayerChoice() {
+    let choice = prompt("Enter: Rock, Paper, or Scissors");
+
+    choice = choice.toLowerCase();
+
+    if(choice === "rock") {
+        return "rock";
+    } 
+    
+    else if(choice === "paper") {
+        return "paper";
+    } 
+    
+    else if(choice === "scissors") {
+        return "scissors";
+    } 
+    
+    else {
+        alert("Invalid Input. Generating a random choice for you.")
+
+        // Since this function returns a random Rock-Paper-Scissors option, I am resuing the function here
+        return getComputerChoice();
+    }
+    
+}
+
 // Main function
 function main() {
     
-    console.log(getComputerChoice());
+    console.log("Computer's choice: " + getComputerChoice());
+    console.log("Player's choice: " + getPlayerChoice());
 }
+
 
 main();
