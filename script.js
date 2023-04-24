26702a0cdfaf661d67de3f162e453dab6ed3144e
jhagg26 <jhagg26@gmail.com>
2023-04-24 09:23:58 -0400

    Remove unused code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Global vars
+// Global vars -- Wasnt quite sure how to pass var's into JS with HTML onclick 
 let playerScore = 0;
 let computerScore = 0;
 let roundNumber = 0;
@@ -161,61 +161,10 @@ function playRound() {
     }
 }
 
-function playGame() {
-
-    let result = "";
-    let playerScore = 0;
-    let computerScore = 0; // This value is just the complement of playerScore, so a var is not needed (computerScore = numRounds - playerScore), we could just compute the value
-    let numRounds = 5;
-
-    for(let i = 0; i < numRounds; i++){
-
-        console.log("Round #" + (i+1));
-
-        let playerChoice = getPlayerChoice();
-        console.log("Player's choice: " + playerChoice);
-
-        let computerChoice = getComputerChoice();
-        console.log("Computer's choice: " + computerChoice);
-    
-        result = playRound(computerChoice, playerChoice);
-
-        console.log(result);
-
-        switch(result.charAt(0)) {
-            case 'P':
-                playerScore++;
-                break;
-            case 'C':
-                computerScore++;
-                break;
-        }
-
-        console.log("-------------------------------------");
-    }
-
-    if(playerScore > computerScore) {
-        console.log("Player wins the series with a score of " + playerScore + " - " + computerScore);
-    }
-
-    else if(playerScore < computerScore) {
-        console.log("Computer wins the series! with a score of " + computerScore + " - " + playerScore);
-    }
-
-    else {
-        console.log("Draw. Both the player and the computer finsihed with a score of " + playerScore + " - " + computerScore);
-    }
-
-    console.log("Refresh the page to play again.");
-    
-}
-
-// Main function
 function main() {
-    
-    //playGame();
-
-    resetGame();
+    playerScore = 0;
+    computerScore = 0;
+    roundNumber = 0;
 }
 
 main();
